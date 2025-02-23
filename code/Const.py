07dcd92e8c22bc216b87dfb687b21324bfc3ba76
Anderson <macedcrazy@gmail.com>
2025-02-23 07:29:52 -0300

    fixing imports in level.py file

--- code/Const.py
+++ code/Const.py
@@ -7,11 +7,11 @@ COLOR_YELLOW = (255,255,0)
 #E
 ENTITY_SPEED = {
     'level1bg0': 0,   # Fundo distante, move muito devagar
-    'level1bg1': 1,   # Camada mais próxima, move devagar
-    'level1bg2': 2,   # Camada intermediária, move um pouco mais rápido
-    'level1bg3': 2.6,   # Camada intermediária mais rápida
-    'level1bg4': 3.6,   # Camada mais rápida
-    'level1bg5': 0,   # Camada mais próxima e mais rápida
+    'level1bg1': 1 ,   # Camada mais próxima, move devagar
+    'level1bg2': 2 ,   # Camada intermediária, move um pouco mais rápido
+    'level1bg3': 3 ,   # Camada intermediária mais rápida
+    'level1bg4': 4 ,   # Camada mais rápida
+    'level1bg5': 5 ,   # Camada mais próxima e mais rápida
 }
 
 
